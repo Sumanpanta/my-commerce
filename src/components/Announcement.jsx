03aea66a69776d52d61347900f6b224ffa2d687e
masterrujino47@gmail.com <masterrujino47@gmail.com>
2023-01-03 08:23:13 +0545

    My5th sem e-commerce project

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Super Offer upto 50% discount on this Christmas</Container>;
+};
+
+export default Announcement;
